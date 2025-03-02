aa0364d0e337572dce367843c4e82b6ad0bb91bf
developer-shajib <sunhailshajib1@gmail.com>
2025-03-02 09:47:48 +0600

    Create todays deals slider

---
+++ hooks/useSettingStore.ts
@@ -0,0 +1,34 @@
+/* eslint-disable no-unused-vars */
+
+import data from '@/lib/data';
+import { create } from 'zustand';
+
+interface SettingState {
+  setting: ClientSetting;
+  setSetting: (newSetting: ClientSetting) => void;
+  getCurrency: () => SiteCurrency;
+  setCurrency: (currency: string) => void;
+}
+
+const useSettingStore = create<SettingState>((set, get) => ({
+  setting: {
+    ...data.settings[0],
+    currency: data.settings[0].defaultCurrency
+  } as ClientSetting,
+  setSetting: (newSetting: ClientSetting) => {
+    set({
+      setting: {
+        ...newSetting,
+        currency: newSetting.currency || get().setting.currency
+      }
+    });
+  },
+  getCurrency: () => {
+    return get().setting.availableCurrencies.find((c: SiteCurrency) => c.code === get().setting.currency) || data.settings[0].availableCurrencies[0];
+  },
+  setCurrency: async (currency: string) => {
+    set({ setting: { ...get().setting, currency } });
+  }
+}));
+
+export default useSettingStore;
